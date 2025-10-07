8ba859d870258c544fa268a8e42fc2c493a795e5
vxxxxc <veper1234@icloud.com>
2025-10-08 01:58:52 +0800

    implement and customize chainlink automation function
    1. implement checkUpkeep function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -36,10 +36,10 @@ abstract contract Raffle is VRFConsumerBaseV2Plus {
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
     RaffleState private s_raffleState;
-    
 
     /* Events */
     event RaffleEntered(address indexed player, uint256 amount);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 _entranceFee,
@@ -47,11 +47,9 @@ abstract contract Raffle is VRFConsumerBaseV2Plus {
         address s_vrfCoordinator,
         bytes32 gasLane,
         uint256 subscriptionId,
-        uint32 callbackGasLimit
-    ) {
+        uint32 callbackGasLimit 
         /* @dev Inheritance Chainlink VRF library, also need to inheritance the library constructor */
-        VRFConsumerBaseV2Plus(s_vrfCoordinator);
-
+    ) VRFConsumerBaseV2Plus(s_vrfCoordinator) {
         i_keyHash = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
@@ -61,7 +59,6 @@ abstract contract Raffle is VRFConsumerBaseV2Plus {
         s_lastTimeStamp = block.timestamp;
 
         s_raffleState = RaffleState.OPEN;
-
     }
 
     function enterRaffle() external payable {
@@ -74,6 +71,28 @@ abstract contract Raffle is VRFConsumerBaseV2Plus {
 
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender, msg.value);
+    }
+
+    /**
+ * @dev This is the function that the Chainlink Keeper nodes will cal to see
+ * if the lottery is ready to have winner picked,
+ * the following should be true in order to for upkeepNeeded to be true:
+ * 1. The time interval has passed between raffle runs.
+ * 2. The lottery is in an "open" state.
+ * 3. The contract has ETH.
+ * 4. Implicity, your subscription is funded with LINK.
+ * @param checkData This is the data the keeper nodes will
+ * @return upkeepNeeded - true if it is time to restart the lottery
+ */
+    function checkUpkeep(bytes calldata checkData )public view returns (bool upkeepNeeded, bytes memory performData ){
+        // Check to see if enough time has passed
+        bool timeHsPassed = ((block.timestamp - s_lastTimeStamp) >= i_interval);
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+
+        upkeepNeeded = timeHsPassed && isOpen && hasBalance && hasPlayers;
+        return (upkeepNeeded, "");
     }
 
     function pickWinner() external {
@@ -92,25 +111,31 @@ abstract contract Raffle is VRFConsumerBaseV2Plus {
                 callbackGasLimit: i_callbackGasLimit,
                 numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
-                    VRFV2PlusClient.ExtraArgsV1({
-                        nativePayment: false
-                    })
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
             });
 
-            uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 _requestId, uint256[] calldata randomWords) internal override {
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata randomWords
+    ) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+
         s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
-        if(!success){
+        if (!success) {
             revert TransferFailed();
         }
 
+        emit WinnerPicked(s_recentWinner);
     }
 
     /* Getter Functions */
