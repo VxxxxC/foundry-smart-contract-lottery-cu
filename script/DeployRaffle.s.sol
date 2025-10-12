874b851221c9adfa651f68809d48a5118835258d
vxxxxc <veper1234@icloud.com>
2025-10-13 01:08:01 +0800

    refactor and fix code , for forge build

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
-import {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
+// import {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle, HelperConfig) {
+    function deployContract() external returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
-        AddConsumer addConsumer = new AddConsumer();
+        // AddConsumer addConsumer = new AddConsumer();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
         // if (config.subscriptionId == 0) {
@@ -25,19 +25,28 @@ contract DeployRaffle is Script {
         //     helperConfig.setConfig(block.chainid, config);
         // }
 
-        vm.startBroadcast(config.account);
+        /**
+             uint256 _entranceFee,
+            uint256 _interval,
+            address s_vrfCoordinator,
+            bytes32 gasLane,
+            uint256 subscriptionId,
+            uint32 callbackGasLimit
+         **/
+
+        vm.startBroadcast();
         Raffle raffle = new Raffle(
-            config.subscriptionId,
-            config.gasLane,
-            config.automationUpdateInterval,
             config.raffleEntranceFee,
-            config.callbackGasLimit,
-            config.vrfCoordinatorV2_5
+            config.automationUpdateInterval,
+            config.vrfCoordinatorV2_5,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
         );
         vm.stopBroadcast();
 
         // We already have a broadcast in here
-        addConsumer.addConsumer(address(raffle), config.vrfCoordinatorV2_5, config.subscriptionId, config.account);
+        // addConsumer.addConsumer(address(raffle), config.vrfCoordinatorV2_5, config.subscriptionId, config.account);
         return (raffle, helperConfig);
     }
 }
